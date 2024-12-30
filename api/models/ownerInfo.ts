e2333d3cbfb9cbbf8d496c9dfa72345118378309
Gang Zhang <gzhangx@hotmail.com>
2024-12-30 11:25:03 -0500

    added owner Id

--- api/models/ownerInfo.ts
+++ api/models/ownerInfo.ts
@@ -2,8 +2,9 @@ import { IDBModel } from './types';
 export const ownerInfo: IDBModel = {
     fields:
         [
-            { field: 'userID', desc: 'Owner', foreignKey: { table: 'userInfo', field: 'userID' }, required: true, isId: true,},
-            { field: 'ownerName',desc: 'Owner Name', required: true, isId: true, },
+            { field: 'userID', desc: 'Owner', foreignKey: { table: 'userInfo', field: 'userID' }, required: true, isId: true, },
+            { field: 'ownerId', desc: 'Owner Id', required: true, isId: true, },
+            { field: 'ownerName',desc: 'Owner Name', required: true,},
             { field: 'taxName', desc: 'Tax Name' },
             { field: 'taxID', desc: 'SSN', },
             { field: 'address', desc: 'Address' },
