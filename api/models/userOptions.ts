786a66c87819296c2e309315fd2a0b7cb0845f68
Gang Zhang <gzhangx@hotmail.com>
2025-02-12 11:48:44 -0500

    fix key

--- api/models/userOptions.ts
+++ api/models/userOptions.ts
@@ -2,7 +2,7 @@ import { IDBModel } from './types';
 export const userOptions = {
     fields:
         [
-            { field: 'id', desc: 'Key Id', required: true,},
+            { field: 'id', desc: 'Key Id', required: true, isId: true},
             { field: 'data', desc: 'Data',  type: 'text'},            
             { field: 'userID', desc: 'Owner ID', isId: true, required: true, isOwnerSecurityField: true, foreignKey: { table: 'userInfo', field: 'userID' } },
         ],    
