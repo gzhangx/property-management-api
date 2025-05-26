ba8a1c882870c5eba6c34703c262b25431d64183
Gang Zhang <gzhangx@hotmail.com>
2025-05-25 21:04:13 -0400

    expensive category refactor

--- api/models/expenseCategories.ts
+++ api/models/expenseCategories.ts
@@ -4,7 +4,9 @@ export const expenseCategories: IDBModel = {
     fields:
         [
             { field: 'expenseCategoryID', desc: 'Id' , required: true, isId: true},
-            { field: 'expenseCategoryName', desc: 'date' },
+            { field: 'expenseCategoryName', desc: 'Name' },
+            { field: 'mappedToTaxExpenseCategoryName', desc: 'What actual tax expense category this maps to' },
+            { field: 'doNotIncludeInTax', type: 'int', desc: 'Do not include this in tax calculation', required: true, def: 0 },
             { field: 'displayOrder', type:'int', desc:'Display Order' }, 
         ]
 };
