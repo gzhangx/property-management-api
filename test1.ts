85e6a8a0e7003fb912ab56217281af6ca20a9a38
Gang Zhang <gzhangx@hotmail.com>
2025-06-13 17:09:22 -0400

    'test'

--- test1.ts
+++ test1.ts
@@ -1,19 +1,15 @@
-import vm from 'vm';
+import { doCiti } from './api/controllers/browserControl';
+import * as db from './api/lib/db'
 
-import Bluebird from 'bluebird';
 async function test() {
-    const context = vm.createContext({});
-    context.Bluebird = Bluebird;
-    context.console = console;
-    console.log('running')
-    vm.runInContext(`
-    async function test1() {
-        console.log('before');
-        await Bluebird.Promise.delay(5000);
-        console.log('after');
-    }
-    test1();
-    `, context);
+    const res = await db.doQueryOneRow('select * from userOptions where id=?', ['testingpwd']);
+    console.log(res.data);    
+    db.end();
+
+    await doCiti('u1', {
+        url: 'https://www.citi.com',
+        password: res.data
+    })
 }
 
 test();
