9b226ffae948e42ed1fa9956acaba4871f28f3e8
Gang Zhang <gzhangx@hotmail.com>
2025-06-14 00:54:59 -0400

    temp

--- test1.ts
+++ test1.ts
@@ -1,15 +1,17 @@
-import { doCiti } from './api/controllers/browserControl';
+//import { doCiti } from './api/controllers/browserControl';
 import * as db from './api/lib/db'
+import { testtestmain } from './api/lib/geckodirect';
 
 async function test() {
     const res = await db.doQueryOneRow('select * from userOptions where id=?', ['testingpwd']);
     console.log(res.data);    
     db.end();
 
-    await doCiti('u1', {
-        url: 'https://www.citi.com',
-        password: res.data
-    })
+    //await doCiti('u1', {
+    //    url: 'https://www.citi.com',
+    //    password: res.data
+    //})
+    await testtestmain(res.data);
 }
 
 test();
