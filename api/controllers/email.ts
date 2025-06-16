b83e054c9af7a56d3866ebfdd026ec88bdd13a43
Gang Zhang <gzhangx@hotmail.com>
2025-06-16 19:57:10 -0400

    pass in user pass

--- api/controllers/email.ts
+++ api/controllers/email.ts
@@ -8,9 +8,12 @@ export async function sendEmail(req: Request, res: Response) {
     if (!req.body) {
         return res.send({ err: "no body" });
     }
-    const { subject, to, html, cc } = req.body;
+    const { user, pass, subject, to, html, cc } = req.body;
     if (!subject || !html) {
         return res.send({ err: "no subject or text" });
+    }
+    if (!user || !pass) {
+        return res.send({ err: "no user or pass" });
     }
 
     const userAuth = getUserAuth(req);
@@ -18,29 +21,33 @@ export async function sendEmail(req: Request, res: Response) {
         return res.send({ err: 'not authorized' });
     }
 
-    const columns = ['googleSmtpUser', 'googleSmtpPass'];
-    const userCfg = await doSqlGetInternal(userAuth, {
-        table: 'userOptions',
-        whereArray: [{
-            field: 'id',
-            op: 'in',
-            val: columns,
-        }]
-    })
+    // const columns = ['googleSmtpUser', 'googleSmtpPass'];
+    // const userCfg = await doSqlGetInternal(userAuth, {
+    //     table: 'userOptions',
+    //     whereArray: [{
+    //         field: 'id',
+    //         op: 'in',
+    //         val: columns,
+    //     }]
+    // })
 
-    const emailCfgAry = columns.reduce((acc, cname) => {
-        const r = userCfg.rows.find(r => r['id'] === cname);
-        if (r) {
-            acc.push(r['data']);
-        }
-        return acc;
-    }, [] as string[]);
-    if (emailCfgAry.length !== 2) {
-        return res.send({ err: 'Please config '+ columns.join(',') });
-    }
+    // const emailCfgAry = columns.reduce((acc, cname) => {
+    //     const r = userCfg.rows.find(r => r['id'] === cname);
+    //     if (r) {
+    //         acc.push(r['data']);
+    //     }
+    //     return acc;
+    // }, [] as string[]);
+    // if (emailCfgAry.length !== 2) {
+    //     return res.send({ err: 'Please config '+ columns.join(',') });
+    // }
+    // const smtpOpts: email.ISmtpConfig = {
+    //     user: emailCfgAry[0],
+    //     pass: emailCfgAry[1],
+    // }
     const smtpOpts: email.ISmtpConfig = {
-        user: emailCfgAry[0],
-        pass: emailCfgAry[1],
+         user,
+         pass,
     }
     //const to = ['a@a.com'];
     //from '"GGBot" <gzhangx@gmail.com>',
